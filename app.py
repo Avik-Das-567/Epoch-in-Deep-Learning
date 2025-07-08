9735c56656437708ebab01f5492be6019a98a653
Avik Das <avikdas567@gmail.com>
2025-07-08 21:15:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+# Data
+X = np.array([1, 2, 3, 4], dtype=float)
+Y = np.array([2, 4, 6, 8], dtype=float)
+
+# Model
+model = Sequential()
+model.add(Dense(units=1, input_shape=[1]))
+
+# Learning setup
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# Train with 100 repetitions (epochs)
+model.fit(X, Y, epochs=100)
+
+# Try predicting 5
+st.write(model.predict(np.array([5.0])))
